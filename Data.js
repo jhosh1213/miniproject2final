68b7718540feacd50237b5003e434767497e5d20
jhosh1213 <87988359+jhosh1213@users.noreply.github.com>
2022-10-20 18:35:50 +0800

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,74 @@
+let shopItemsData = [
+    {
+      id: "lebron19",
+      name: "Nike Lebron 19 Low 'Beyond the Seas'",
+      price: 11995,
+      img: "images/product1.jpg",
+    },
+    {
+      id: "nikeAirforce1",
+      name: "Nike Air Force 1 Mid React “Summit White”",
+      price: 9995,
+      img: "images/product2.jpg",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "Air Jordan 1 Low “Vintage Grey”",
+      price: 9995,
+      img: "images/product3.jpg",
+    },
+    {
+      id: "thyfhc32bcv",
+      name: "Air Jordan 4 Retro SE “Black Canvas”",
+      price: 14995,
+      img: "images/product4.jpg",
+    },
+    {
+        id: "thyf513h3cbcv",
+        name: "Adidas Yeezy Boost 350 v2 “Slate”",
+        price: 22995,
+        img: "images/product5.jpg",
+      },
+      {
+        id: "thyfh3c323bcv",
+        name: "Adidas Stan Smith “Cloud White/Gum”",
+        price: 6995,
+        img: "images/product6.jpg",
+      },
+      {
+        id: "thyfhcbdscv",
+        name: "Nike Air Force 1 Low SE “Celestine Blue”",
+        price: 8495,
+        img: "images/product7.jpg",
+      },
+      {
+        id: "thyfhdasd3cbcv",
+        name: "Nike Dunk Low Disrupt 2 “Pink Oxford”",
+        price: 13495,
+        img: "images/product8.jpg",
+      },
+      {
+        id: "thyfhcd232asdbcv",
+        name: "Air Jordan 1 Low “Pastel Purple”",
+        price: 12995,
+        img: "images/product9.jpg",
+      },
+      {
+        id: "thyfhcdsdsasdadabcv",
+        name: "Air Jordan 11 “Pure Violet”",
+        price: 13495,
+        img: "images/product10.jpg",
+      },
+      {
+        id: "thyfhdasdacsdbcv",
+        name: "Adidas Yeezy 350 v2 “Mx Oat”",
+        price: 22995,
+        img: "images/product11.jpg",
+      },
+      {
+        id: "thyfhdasdcdasbcv",
+        name: "Adidas Tyshawn Womens “UNC”",
+        price: 6995,
+        img: "images/product12.jpg",
+      },
+  ];
